d15f34def7462c75d1fa16bb6d1ed8acd93da014
phillipphoenix <pphoelich@gmail.com>
2013-09-21 09:03:11 -0700

    Changed the methods back, since I found out that we could use the readline library to do history as well. I don't exactly know how that works yet with moving back and forth though.

--- CShell.c
+++ CShell.c
@@ -17,13 +17,6 @@
 /* --- symbolic constants --- */
 #define MAXBUF 512
 
-/* --- command history entry struct --- */
-typedef struct _hisEntry {
-  char *entry;
-  struct _hisEntry *prev;
-  struct _hisEntry *next;
-} HisEntry;
-
 /* --- use the /proc filesystem to obtain the hostname --- */
 char *gethostname()
 {
@@ -44,27 +37,6 @@ char *gethostname()
 
 }
 
-/* --- save the commands to history --- */
-int historifyCmd(char *cmdline, HisEntry *iterator) {
-  if (iterator != 0) {
-    iterator->next = malloc(sizeof(struct HisEntry));
-    HisEntry *prev = iterator; // Save the current entry for prev.
-    iterator = iterator->next;
-    // Only continue if not out of memory.
-    if (iterator != 0) {
-      iterator->prev = prev;
-      iterator->next = 0;
-      iterator->entry = cmdline;
-    } else {
-      // Return 2 if out of memory.
-      return 2;
-    }
-    return 0;
-  }
-  // The iterator pointer didn't point to anything.
-  return 1;
-}
-
 /* --- execute a shell command --- */
 int executeshellcmd (Shellcmd *shellcmd)
 {
@@ -81,14 +53,6 @@ int main(int argc, char* argv[]) {
   char hostname[MAXBUF];
   int terminate = 0;
   Shellcmd shellcmd;
-
-  /* initialize the shell history */
-  HisEntry *root; // Root history entry.
-  root = malloc(sizeof(struct HisEntry));
-  root->prev = 0;
-  root->next = 0;
-  root->entry = "";
-  HisEntry *iterator = root; // The current history entry (start at root).
   
   if (gethostname(hostname)) {
 
@@ -97,7 +61,7 @@ int main(int argc, char* argv[]) {
       printf("%s", hostname);
       if (cmdline = readline(":# ")) {
 	if(*cmdline) {
-	  historifyCmd(cmdline, iterator); // Save the cmd to history.
+	  add_history(cmdline);
 	  if (parsecommand(cmdline, &shellcmd)) {
 	    terminate = executeshellcmd(&shellcmd);
 	  }
